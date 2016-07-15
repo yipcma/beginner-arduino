d3419931559fb7e6b625377dcc4a8e96ed6be2f1
Andrew Yip <yipcma@gmail.com>
2016-07-15 22:00:21 +0300

    initial commit with p1: blinking

---
+++ p1-1_blinking_an_led/p1-1_blinking_an_led.ino
@@ -0,0 +1,14 @@
+//Project -- Blinking an LED
+/*
+ * Description: turn LED on and off every other second.
+ */
+int ledPin = 10;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
