719b2143308a7366660f138d4b3c5efd4ef84215
Andrew Yip <yipcma@gmail.com>
2016-08-05 04:10:25 +0300

    p6ex2 doorbell in A

---
+++ p6-ex2_door_bell/p6-ex2_door_bell.ino
@@ -0,0 +1,20 @@
+// p6-ex2 door bell
+
+const int toneVal = 220;
+const int buzzPin = 8;
+const int button = 9;
+
+void setup() {
+  pinMode(buzzPin, OUTPUT);
+  pinMode(button, INPUT);  
+}
+
+void loop() {
+  int state = digitalRead(button);
+
+  if (state == HIGH) {
+    tone(buzzPin, toneVal, 500);
+    delay(1000);
+  }
+}
+
